aae69e869e1f4e9d63f4508563ec669fc546b999
senthuran16 <senthuranrc16@gmail.com>
2017-08-09 23:27:57 +0530

    implemented java object to json in TemplateManagerHelper

--- src/main/java/core/Template.java
+++ src/main/java/core/Template.java
@@ -13,4 +13,14 @@ public class Template {
     private String javascript; // todo: revise whether String or not
     private Collection<String> siddhiApps;
     private Map<String, Map<String, String>> properties; // { {propertyName : {defaultValue:xxx, type:yyy}} , ...}
+
+    // TODO : rough
+    public Template(String name, int maximumInstances, String description, String javascript, Collection<String> siddhiApps, Map<String, Map<String, String>> properties) {
+        this.name = name;
+        this.maximumInstances = maximumInstances;
+        this.description = description;
+        this.javascript = javascript;
+        this.siddhiApps = siddhiApps;
+        this.properties = properties;
+    }
 }
--- src/main/java/core/TemplateManagerHelper.java
+++ src/main/java/core/TemplateManagerHelper.java
@@ -1,11 +1,41 @@
 package core;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 /**
  * Consists of methods for additional features for the exposed root.Template Manager service
  */
 public class TemplateManagerHelper {
+
+    public static void main(String[] args) {
+        ArrayList<String> a = new ArrayList<String>();
+        a.add("siddhiApp1");
+        a.add("siddhiApp2");
+
+        Map<String,String> m1 = new HashMap<String, String>();
+        m1.put("defaultValue","TestTemperature");
+        m1.put("type","String");
+        Map<String,String> m2 = new HashMap<String, String>();
+        m2.put("defaultValue","SampleDescription");
+        m2.put("type","String");
+
+        Map<String,Map<String,String>> m = new HashMap<String, Map<String, String>>();
+
+        m.put("sensorName",m1);
+        m.put("sensorDescription",m2);
+
+
+        Template tem = new Template("tem1",7,"Sample template","<javascript",a,m);
+
+        TemplateManagerHelper t = new TemplateManagerHelper();
+        System.out.println(t.templateToJson(tem));
+    }
 
     /**
      * Converts a JSON file to a root.Template object
@@ -21,9 +51,14 @@ public class TemplateManagerHelper {
      * Converts a root.Template object to a JSON file
      * @param template Given root.Template object
      */
-    public void templateToJson(Template template){
+    public String templateToJson(Template template){
         // todo: refer gson and decide return type
         // todo: implement
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+        String jsonObject = gson.toJson(template);
+
+        return jsonObject;
     }
 
     /**
@@ -40,8 +75,13 @@ public class TemplateManagerHelper {
      * Converts a Business Rule object to a JSON file
      * @param businessRule Given Business Rule object
      */
-    public void businessRuleToJson(BusinessRule businessRule){
+    public String businessRuleToJson(BusinessRule businessRule){
         // todo: implement
         // todo : refer gson and decide return type
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+        String jsonObject = gson.toJson(businessRule);
+
+        return jsonObject;
     }
 }
